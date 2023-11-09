b1682a735a411730b489ba00cffd00c2ccd9e44a
Sanyam <sanyamjha19@gmail.com>
2023-11-09 18:29:04 +0530

    updated library, deleted test files & added examples

--- run_usr.py
+++ run_usr.py
@@ -1,7 +1,15 @@
 from usr import USR
+import sys
 
-input_dir = "./data"	#change to data storage location
-output_dir = "./results_test"	#change to result storage location
+n = len(sys.argv)
+
+input_dir = str(sys.argv[1])
+output_dir = str(sys.argv[2])
+
+if input_dir == "":
+	input_dir = "./test"	#change to data storage location
+if output_dir == "":
+	output_dir = "./results_test"	#change to result storage location
 
 usr = USR()
 usr.set_root_folder_path(input_dir)
--- usr.py
+++ usr.py
@@ -2,186 +2,314 @@ import os
 from wxconv import WXC
 
 class USR:
-		def __init__(self) -> None:
-				self.markers = {"Ora", "evaM", "waWA", "agara", "yaxi", "wo", "kyoMki",
-					"isIlie","jabaki","yaxyapi","waWApi","yaxyapi","Pira BI",
-					"lekina","kiMwu","paraMwu","jaba","waba","yA","aWavA"}
-				
-				self.discourse_relation = {
-						"Ora" : "samuccaya",
-						"evaM" : "samuccaya",
-						"waWA" : "samuccaya",
-						"agara" : "AvaSyakwA-pariNAma",
-						"yaxi" : "AvaSyakwA-pariNAma",
-						"wo" : "AvaSyakwA-pariNAma",
-						"kyoMki" : "kArya-kAraNa",
-						"isIlie" : "kArya-kAraNa",
-						"jabaki" : "vyABicAra",
-						"yaxyapi" : "vyABicAra",
-						"waWApi" : "vyABicAra",
-						"Pira BI" : "vyABicAra",
-						"lekina" : "viroXi",
-						"kiMwu" : "viroXi",
-						"paraMwu" : "viroXi",
-						"jaba" : "samAnakAla",
-						"waba": "samAnakAla",
-						"yA": "anyawra",
-						"aWavA": "anyawra",
-				}
-				
-				self.discourse_pos = {
-					"samuccaya" : "1",
-					"anyawra": "1",
-					"samAnakAla":"0",
-					"viroXi": "1",
-					"vyABicAra":"0",
-					"kArya-kAraNa": "0",
-					"AvaSyakwA-pariNAma": "0"
-				}
-				
-				self.con = WXC(order='utf2wx')
-				self.res_folder_path = ""
-				self.root_folder_path = ""
+	"""
+		This class contains various methods which facilitate in discourse prediction
 		
-		
-		def set_res_folder_path(self, res_path):
-			self.res_folder_path = res_path
+		Due to the nature of implementation of this file, it is expected to store 
+		the USRs in a 3-level heirarchy, while passing the path of the root folder
+		e.g.  : root -> sub_folder_1 -> usr_file.txt
+	"""
+	def __init__(self) -> None:
+			#set of single word markers
+			self.markers = {"Ora", "evaM", "waWA", "agara", "yaxi", "wo", "kyoMki",
+				"isIlie","jabaki","yaxyapi","waWApi","yaxyapi","Pira BI",
+				"lekina","kiMwu","paraMwu","jaba","waba","yA","aWavA", 'viparIwa'}
 			
-		def get_res_folder_path(self):
-			return self.res_folder_path
+			#set of multi words marker
+			self.multi_word_markers = {"isake pariNAma svarupa", 'isake viparIwa'}
+			
+			#maps all the markers to respective discourse relations
+			self.discourse_relation = {
+					"Ora" : "samuccaya",
+					"evaM" : "samuccaya",
+					"waWA" : "samuccaya",
+					"agara" : "AvaSyakwA-pariNAma",
+					"yaxi" : "AvaSyakwA-pariNAma",
+					"wo" : "AvaSyakwA-pariNAma",
+					"kyoMki" : "kArya-kAraNa",
+					"isIlie" : "kArya-kAraNa",
+					"jabaki" : "vyABicAra",
+					"yaxyapi" : "vyABicAra",
+					"waWApi" : "vyABicAra",
+					"Pira BI" : "vyABicAra",
+					"lekina" : "viroXi",
+					"kiMwu" : "viroXi",
+					"paraMwu" : "viroXi",
+					"jaba" : "samAnakAla",
+					"waba": "samAnakAla",
+					"yA": "anyawra",
+					"aWavA": "anyawra",
+					'isake pariNAma svarupa' : "vyABicAra",
+					'isake viparIwa' : "viroXi",
+					"viparIwa" : 'viroXi'
+			}
+			
+			#maps whether the discourse relation is to be 
+			#concatenated to current USR or previous USR
+			self.discourse_pos = {
+				"samuccaya" : "1",
+				"anyawra": "1",
+				"samAnakAla":"0",
+				"viroXi": "1",
+				"vyABicAra":"0",
+				"kArya-kAraNa": "1",
+				"AvaSyakwA-pariNAma": "0"
+			}
+			
+			#module to convert from devnagri to wx
+			self.convert_to_wx = WXC(order='utf2wx')
+			self.res_folder_path = ""
+			self.root_folder_path = ""
+	
+	
+	def set_res_folder_path(self, res_path):
+		"""
+			sets the path where the processing results 
+			will be stored
+		"""
+		self.res_folder_path = res_path
+		
+	def get_res_folder_path(self):
+		"""
+			returns the path to results folder
+		"""
+		return self.res_folder_path
 
-		def set_root_folder_path(self, root_path):
-			self.root_folder_path = root_path
-		
-		def get_root_folder_path(self):
-			return self.root_folder_path
-		
-		def create_res_folder(self, path):
-			try:
-				os.makedirs(path)
-			except FileExistsError:
-				print("error, folder already exists")
-				# filename += str(random.randint(0,100))
-				# path = os.path.join(parent_dir, filename)
-				# os.makedirs(path)
-			return path		
-		
-		def convert_to_usr(self, file_path):
-			with open(file_path, 'r') as file:
-					content = file.read()
-					return [string.split(",") for string in content.split("\n")]
-					# print(content_usr_list)
-		
-		def save_usr_to_txt(self, prev_usr, prev_filename,  sub_folder_path):
-			with open(sub_folder_path + "/" + prev_filename + '.txt', "w") as file:
-					for usr_entry in prev_usr:
-							for item in usr_entry:
-									if item == None:
-										item = ""
-									file.write(item + ",")
-							file.write("\n")
-					file.close()
-			print("data written")
-		
-		# returns discourse relation for a given string (use curr_usr string)
-		def mpd(self,str2):
-			words1 = str2.split()
-			for word in words1:
-				if word in self.markers:
-					#print(word)
-					return self.discourse_relation[word]
+	def set_root_folder_path(self, root_path):
+		"""
+			sets the path where the USRs are stored
+		"""
+		self.root_folder_path = root_path
+	
+	def get_root_folder_path(self):
+		"""
+			returns the path to root folder
+		"""
+		return self.root_folder_path
+	
+	def create_res_folder(self, path):
+		"""
+			in case a result folder is not yet created at the 
+			specified path, it creates such a folder
+		"""
+		try:
+			os.makedirs(path)
+		except FileExistsError:
+			print("Folder already exists!")
+		return path		
+	
+	def convert_to_usr(self, file_path):
+		"""
+			it opens the file content (USR) & converts it 
+			from a single string to a list of rows
 			
-			return "-1"
-
-		def get_main_str(self, usr):
-			strings_with_main = ""
-			i = 1
-			for item in usr[5]:
-				if item == "0:main":
-					strings_with_main += str(i)
-				i+=1
-			return strings_with_main, i
+			- file\\_path: path to USR file
+		"""
+		with open(file_path, 'r') as file:
+				content = file.read()
+				USR_list = content.split("\n")
+				for i in range(len(USR_list)):
+					if i == 0:
+						# USR_list[i] = USR_list[i].split(" ")
+						continue
+					USR_list[i] = USR_list[i].split(',')
+				return USR_list
+	
+	def save_usr_to_txt(self, usr, filename,  sub_folder_path):
+		"""
+			it converts the updated USR from list to string 
+			and writes it in a .txt file
 			
-		def process_usr(self,prev_filename, prev_usr, curr_filename, curr_usr):
-				str1 = curr_usr[0][0][1:]
-				str1 = self.con.convert(str1)
-				# print("str: " , self.con.convert(str1))
-				discourse_rel = self.mpd(str1)
-				if(discourse_rel == "-1"):
-					return prev_usr, curr_usr
-				y = self.discourse_pos[discourse_rel]
-				# y == 0  means discourse relation to be added to prev_usr
-				# y == 1  means discourse relation to be added to curr_usr
-				if y == 0:
-					# find main of curr_usr
-					strings_with_main, iii = self.get_main_str(curr_usr)
-					usr_id = curr_filename
-					fin = usr_id + "." + strings_with_main + ":" + discourse_rel
-					position_of_discourse = iii
-					print(fin)
-					try:
-						prev_usr[6][position_of_discourse] = fin
-					except IndexError:
-						li = [None] * (iii + 1)
-						li[iii] = fin
-						prev_usr[6] = li
-					# fin = usr_id + "." + strings_with_main
+			- usr : previous USR (list) object \n
+			- filename : name of the file \n
+			- sub\\_folder\\_path : path where the file was stored in root structure \n
+		"""
+		with open(sub_folder_path + '/' + filename + '.txt', 'w') as file:
+			for row_number in range(len(usr)):
+				output_string = ""
+				if(row_number == 0):
+					output_string = usr[row_number]
 				else:
-					strings_with_main, iii = self.get_main_str(prev_usr)
-					usr_id = prev_filename
-					usr_id = usr_id[:-4]
-					fin = usr_id + "." + strings_with_main + ":" + discourse_rel
-					position_of_discourse = iii
-					print(fin, iii)
-					try:
-						curr_usr[6][position_of_discourse] = fin
-					except IndexError:
-						li = [None] * (iii + 1)
-						li[iii] = fin
-						curr_usr[6] = li
-				
-				return prev_usr, curr_usr
-		
-		def run(self):
-			
-			self.create_res_folder(self.res_folder_path)
-			for folder_name, sub_folder, filenames in os.walk(self.root_folder_path):
-				print("foldername ", folder_name)
-				print("subfolder", sub_folder)
-				print("filename: " , filenames)
-				
-				if sub_folder.__sizeof__ != 0:
-					
-					for sub_folder_name in sub_folder:
-						sub_folder_path = self.root_folder_path +  "/" + sub_folder_name
-						# print(sub_folder_path)
-						path = self.res_folder_path + "/" + sub_folder_name 
-						self.create_res_folder(path)
-						for _, _, filenames in os.walk(sub_folder_path): 
-							filenames = sorted(filenames)
-							
-							prev_usr = []
-							curr_usr = []
-							prev_filename = "0"
-							for filename in filenames:
-								file_path	= sub_folder_path + "/" + filename
+					for element in usr[row_number]:
+						if element == None:
+							element = ""
+						output_string += element + ','
 						
-								with open(file_path, "r") as file:
-										content = file.read()
-										print(filename, ":->", len(content), ":->>", content.find("\n"))
+				file.write(output_string[:-1] + '\n')
+		file.close()
+
+	def get_main_str(self, usr):
+		"""
+			returns position of the "0:main" string in the current element
+			- usr : takes the USR list as input
+		"""
+		for position in range(len(usr[5])):
+			word = usr[5][position]
+			if word == "0:main":
+				return position
+	
+	def get_discourse_from_word(self, sentence):
+		"""
+			get discourse relation from the first word\n
+			- sentence : list of first 5 words in the wx converted sentence
+		"""
+		sent = sentence[0]
+		
+		for i in range(len(sentence)-1):
+			if sent in self.markers or sent in self.multi_word_markers:
+				return self.discourse_relation[sent]
+			sent += " " + sentence[i+1]
+		return "-1"
+	
+	def process_usr(self,prev_filename, prev_usr, curr_filename, curr_usr):
+			"""
+				process USR
+				
+					-	prev\\_filename : filename of the previous USR file
+					- prev\\_usr : previous USR list
+					- curr\\_filename : filename of the current USR file
+					- curr\\_usr : current USR list
+			"""
+
+			sentence_without_hash = curr_usr[0][1:]	#removing '#' symbol
+			sentence_without_hash = self.convert_to_wx.convert(sentence_without_hash) #converting to wx notation
+			
+			#taking the first 5 words from the beginning 
+			#of the sentence and arranges them in a list
+			sentence_without_hash = sentence_without_hash.split(" ")[:5]	
+
+			discourse_relation_from_sentence = self.get_discourse_from_word(sentence_without_hash)	#gets the discourse relation from the word list 
+			
+			if(discourse_relation_from_sentence == "-1"):	#if no discourse relation found, return the USR lists as it is
+				return prev_usr, curr_usr
+			
+			select_usr_to_append = self.discourse_pos[discourse_relation_from_sentence]	#get the USR list where the discourse relation is to be appended.
+																						#select_usr_to_append == 0  means discourse relation to be added to prev_usr
+																						#select_usr_to_append == 1  means discourse relation to be added to curr_usr
+
+			pos_main_prev_usr = self.get_main_str(prev_usr)	#find position of 0:main in previous usr list
+			pos_main_curr_usr = self.get_main_str(curr_usr)	#find position of 0:main in current usr list
+			print("val: ",sentence_without_hash, discourse_relation_from_sentence, pos_main_curr_usr, pos_main_prev_usr, select_usr_to_append)
+			if select_usr_to_append == "1":
+				"""
+					if the USR to be updated is the current USR list,
+					then we need to append the usr id of previous file 
+					e.g. :
+					
+						merI pehlI gADZI coTI WI  lekina xUsarI gAdZI Limousine ke AkAra kI hE
+						2a
+						merI pehlI gADZI coTI WI
+						speaker,pehlA_1,gADZI_1,CoTI_1,hE_1-pres
+						1,2,3,4,5
+						anim,,,,
+						[m sg m],,[- sg a],,
+						2:r6,3:ord,5:k1,5:k1s,0:main
+						,,,,
+						,,,,
+						,,,,
+						affirmative
+
+						2b
+						lekina xUsarI gAdZI Limousine ke AkAra kI hE
+						xUsarA_1,gAdZI_1,limousine, AkAra_1, hE_1-pres
+						1,2,3,4,5
+						,,ne,,
+						[- sg a],[-  sg a],[- sg a],
+						2:ord,5:k1,5:r6,? 0:main
+						,,,,2a.5:viroXI
+						,,,,
+						,,,,
+						affirmative
+				"""
+				print(" add ", curr_usr[6], pos_main_curr_usr)
+				usr_id = prev_filename	
+				final_string_to_append = usr_id + '.' + str(pos_main_prev_usr + 1) + ':' + discourse_relation_from_sentence
+				curr_usr[6][pos_main_curr_usr] = final_string_to_append
+			else:
+				"""
+					if the usr to be updated is previous USR list,
+					then we need to append the USR ID of the current file
+					
+					e.g.:
+						1a
+						#Apa cAhawe hEM
+						addressee,cAha_1-wA_hE_1
+						1,2
+						anim,
+						[m sg u],
+						2:k1,0:main
+						,1b.4:AvaSaykwA-parinAma
+						,
+						affirmative
+						
+
+						1b
+						#wo meM Apake Gara AuzgA
+						speaker,addressee,Gara_1,A_1-gA_1
+						1,2,3,4
+						anim,,,
+						[- sg u],,[- sg a],
+						4:k1,3:r6,4:k2p,0:main
+						,,,
+						,respect,,
+						,,,
+						affirmative
+				"""
+				print(prev_usr[6], pos_main_prev_usr)
+				usr_id = curr_filename
+				final_string_to_append = usr_id + '.' + str(pos_main_curr_usr + 1) + ':' + discourse_relation_from_sentence
+				prev_usr[6][pos_main_prev_usr] = final_string_to_append
+				# try:
+				# except IndexError:
+				# 	print("error : ", prev_usr[6], pos_main_prev_usr, final_string_to_append)
+					
+			return prev_usr, curr_usr
+	
+	def run(self):
+		"""
+			traverses root folder & calls crucial methods required to implement
+			rule-based discourse prediction in all existing USRs inside the folder.
+		
+			Due to the nature of implementation of this file, it is expected to store 
+			the USRs in a 3-level heirarchy, while passing the path of the root folder
+			e.g.  : root -> sub_folder_1 -> usr_file.txt
+		"""
+		
+		#create the res folder or print if the folder already exists
+		self.create_res_folder(self.res_folder_path)
+		
+		#traverse to the subfolders inside root folder
+		for _, sub_folder, filenames in os.walk(self.root_folder_path):
+			if sub_folder.__sizeof__ != 0:
+				
+				for sub_folder_name in sub_folder:
+					sub_folder_path = self.root_folder_path +  "/" + sub_folder_name
+					path = self.res_folder_path + "/" + sub_folder_name 
+					self.create_res_folder(path)
+					
+					#traverse all the files inside the subfolder
+					for _, _, filenames in os.walk(sub_folder_path): 
+						filenames = sorted(filenames)
+						prev_usr = []
+						curr_usr = []
+						prev_filename = "0"
+						
+						#for each file, try rule-based discourse prediction
+						for filename in filenames:
+							file_path	= sub_folder_path + "/" + filename
+							
+							with open(file_path, "r") as file:
+									content = file.read()
+									
+									if content.find("\n") != -1:	#content not empty
+										curr_usr = self.convert_to_usr(file_path) #converts the file content into a list object. This creates a USR list.
+										prev_usr, curr_usr = self.process_usr(prev_filename, prev_usr, filename, curr_usr)
+										self.save_usr_to_txt(prev_usr, prev_filename, path)
+										self.save_usr_to_txt(curr_usr, filename, path)
+										prev_filename = filename
+										prev_usr = curr_usr  #current usr becomes the PREVIOUS USR for future file
 										
-										if content.find("\n") != -1:
-											curr_usr = self.convert_to_usr(file_path)
-											prev_usr, curr_usr = self.process_usr(prev_filename, prev_usr, filename, curr_usr)
-											self.save_usr_to_txt(prev_usr, prev_filename, path)
-											self.save_usr_to_txt(curr_usr, filename, path)
-											prev_filename = filename
-											prev_usr = curr_usr
-											
-											# fn(prev_usr, curr_usr)
-											
-										# content.find("\n")
-										# hindi_str = content.partition("\n")
-										# print(hindi_str)
-							break
+						#removes the dummy file thus created
+						os.remove(self.res_folder_path + "/" + sub_folder_name + "/" + "0.txt")
+						break
 			
