47954a32457773a638c1e69ac2c6fe4d7e90d861
Sanyam <sanyamjha19@gmail.com>
2023-12-23 17:35:55 +0530

    documentation

--- run_usr.py
+++ run_usr.py
@@ -1,20 +1,37 @@
 from usr import USR
 import sys
 
-n = len(sys.argv)
+def main():
+		"""
+		Main function to run the USR algorithm.
 
-input_dir = str(sys.argv[1])
-output_dir = str(sys.argv[2])
-input_mode = int(sys.argv[3])
+		Usage: python script.py [input_directory] [output_directory] [input_mode]
 
-if input_dir == "":
-	input_dir = "./test"	#change to data storage location
-if output_dir == "":
-	output_dir = "./results_test"	#change to result storage location
+		Parameters:
+		- input_directory (str): Path to the input directory containing data.
+		- output_directory (str): Path to the output directory for storing results.
+		- input_mode (int): Input mode, 0 for a single root folder, 1 if root folder contains subfolders.
 
-usr = USR()
-usr.set_root_folder_path(input_dir)
-usr.set_res_folder_path(output_dir)
-usr.set_input_mode(input_mode)	# set input mode == 0 for single root folder
-																# in case root folder contains sub folders, use input mode 1
-usr.run()
+		If command-line arguments are not provided, default values will be used.
+		"""
+		n = len(sys.argv)
+
+		input_dir = str(sys.argv[1])
+		output_dir = str(sys.argv[2])
+		input_mode = int(sys.argv[3])
+
+		if input_dir == "":
+			input_dir = "./test"	#change to data storage location
+		if output_dir == "":
+			output_dir = "./results_test"	#change to result storage location
+
+		usr = USR()
+		usr.set_root_folder_path(input_dir)
+		usr.set_res_folder_path(output_dir)
+		usr.set_input_mode(input_mode)	# set input mode == 0 for single root folder
+																		# in case root folder contains sub folders, use input mode 1
+		usr.run()
+
+
+if __name__ == "__main__":
+    main()
